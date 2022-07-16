d160ec539f53f4d9e8478e5172cf59e58a08b330
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-07-16 16:38:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const arr = [{key :"relativeListItem",name : "Ricky"},
-{key :"relativeListItem", name : "Chinnu"},
-{key :"relativeListItem", name : "Alex"},
-{key :"relativeListItem", name : "Caesar"}]
+const arr = [{key :"relativeListItem1",name : "Ricky"},
+{key :"relativeListItem2", name : "Chinnu"},
+{key :"relativeListItem3", name : "Alex"},
+{key :"relativeListItem4", name : "Caesar"}]
 
 class App extends Component {
   
     render() {
         
-        const relativeList = arr.map((item,idx)=>
-             <li key={item.key+idx}>{item.name}</li> )
+        const relativeList = arr.map((item)=>
+             <li key={item.key}>{item.name}</li> )
 
         return(
             <div id="main">
